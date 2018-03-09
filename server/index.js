2bd9788fe83f2b54a22eb283a4aa3b7729ff8b70
Marcel Hinderlich <m.hinderlich@reply.de>
2018-03-09 23:46:09 +0100

    first change

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+  res.send({ hi: 'there homeboy' });
 });
 
 const PORT = process.env.PORT || 8080;
